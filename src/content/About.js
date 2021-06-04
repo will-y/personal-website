a807c273bb8a2d02d3747d1d1d88bd99ced2386e
Will Yelton <willhyelton@gmail.com>
2021-06-04 11:18:07 -0400

    Added contact page

--- src/content/About.js
+++ src/content/About.js
@@ -3,9 +3,7 @@ import React from 'react';
 class About extends React.Component {
     render() {
         return (
-            <div>
-                <p>About Component</p>
-            </div>
+            <p>About Component</p>
         );
     }
 }
--- src/content/Contact.js
+++ src/content/Contact.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import github from '../resources/images/contact/github.png';
+import li from '../resources/images/contact/li.png';
+import email from '../resources/images/contact/email.png';
+import './Contact.css';
 
 class Contact extends React.Component {
     render() {
         return (
-            <div>
-                <p>Contact Component</p>
+            <div className="contact-page-container">
+                <h2 className="info">You can contact me at any of the following (email is preferred):</h2>
+                <div className="contact-container">
+                    <a className="contact-box" href="mailto: yeltonwh@rose-hulman.edu" draggable={false}>
+                        <div className="contact-content">
+                            <img className="contact-image" src={email} />
+                            <p className="contact-text">yeltonwh@rose-hulman.edu</p>
+                        </div>
+                    </a>
+                    <a className="contact-box" href="https://github.com/will-y" target="_blank" rel="noreferrer" draggable={false}>
+                        <div className="contact-content">
+                            <img className="contact-image" src={github} />
+                            <p className="contact-text">Will-Y On Github</p>
+                        </div>
+                    </a>
+                    <a className="contact-box" href="https://www.linkedin.com/in/will-yelton/" target="_blank" rel="noreferrer" draggable={false}>
+                        <div className="contact-content">
+                            <img className="contact-image" src={li} />
+                            <p className="contact-text">LinkedIn</p>
+                        </div>
+                    </a>
+                </div>
             </div>
         );
     }
