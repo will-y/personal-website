b648e1523dd69650afd20b930c99353b5058e2e2
Will Yelton <willhyelton@gmail.com>
2021-06-04 11:35:50 -0400

    Fixed scrolling error on contacts page

--- src/content/About.js
+++ src/content/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 class About extends React.Component {
     render() {
         return (
-            <p>About Component</p>
+            <p>About Component(WIP)</p>
         );
     }
 }
--- src/content/Home.js
+++ src/content/Home.js
@@ -4,7 +4,7 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-                <p>Home Component</p>
+                <p>Home Component (WIP)</p>
             </div>
         );
     }
